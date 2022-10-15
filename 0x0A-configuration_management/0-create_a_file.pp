cc308cd27b49513e729b1993ee70c42717ad979c
yalem123 <yalembrhankelayneh@gmail.com>
2022-10-14 17:26:00 -0700

    task1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with specific requirment
+
+file { 'school'
+ path => '/tmp/school',
+ permission => '0744',
+ owner => 'www-data',
+ group => 'www-data',
+ contains => 'I love Puppet',
+}
